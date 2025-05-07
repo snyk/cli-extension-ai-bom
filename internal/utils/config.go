cec987dfb37c1b89667c43f2a48c1df455f74109
sburuiana-snyk <sebastian.buruiana@snyk.io>
2025-05-07 10:31:53 +0000

    chore: fixed error introduced by merge

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -1,12 +1,7 @@
 package utils
 
 const (
-<<<<<<< HEAD
 	FlagExperimental                    = "experimental"
+	FlagSkipDepGraph                    = "skip-dep-graph"
 	ConfigurationSnykCodeClientProxyURL = "SNYK_CODE_CLIENT_PROXY_URL"
-=======
-	FlagExperimental            = "experimental"
-	FlagSkipDepGraph            = "skip-dep-graph"
-	ConfigurationSnykCodeAPIURL = "snyk_code_api_url"
->>>>>>> e6a7076 (chore: gate dep graphs behind config flag)
 )
