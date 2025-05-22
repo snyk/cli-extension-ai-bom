e9d86c26d4765f4f0124dc18912bca2eb2e36322
Evgenii Kotelnikov <evgeny.kotelnikov@gmail.com>
2025-05-22 15:56:48 +0200

    feat: add the --html flag (#20)

--- internal/commands/aibomcreate/aibomcreate.go
+++ internal/commands/aibomcreate/aibomcreate.go
@@ -4,6 +4,8 @@ import (
 	goErrors "errors"
 	"fmt"
 	"os"
+	"strings"
+	"text/template"
 
 	"github.com/rs/zerolog"
 
@@ -15,6 +17,8 @@ import (
 	"github.com/snyk/cli-extension-ai-bom/internal/services/depgraph"
 	"github.com/snyk/cli-extension-ai-bom/internal/utils"
 
+	_ "embed"
+
 	"github.com/spf13/pflag"
 )
 
@@ -23,6 +27,7 @@ var WorkflowID = workflow.NewWorkflowIdentifier("aibom")
 func RegisterWorkflows(e workflow.Engine) error {
 	flagset := pflag.NewFlagSet("snyk-cli-extension-ai-bom", pflag.ExitOnError)
 	flagset.Bool(utils.FlagExperimental, false, "This is an experiment feature that will contain breaking changes in future revisions")
+	flagset.Bool(utils.FlagHTML, false, "Output the AI BOM in HTML format instead of JSON")
 
 	configuration := workflow.ConfigurationOptionsFromFlagset(flagset)
 	if _, err := e.Register(WorkflowID, configuration, AiBomWorkflow); err != nil {
@@ -36,6 +41,9 @@ func AiBomWorkflow(invocationCtx workflow.InvocationContext, _ []workflow.Data) 
 	depGraphService := depgraph.NewDepgraphServiceImpl()
 	return RunAiBomWorkflow(invocationCtx, codeService, depGraphService)
 }
+
+//go:embed aibom.html
+var htmlTemplate string
 
 func RunAiBomWorkflow(
 	invocationCtx workflow.InvocationContext,
@@ -88,19 +96,37 @@ func RunAiBomWorkflow(
 	}
 
 	logger.Debug().Msg("Successfully generated AI BOM document.")
-	return aiBomDoc, nil
+	workflowData := newWorkflowData("application/json", []byte(aiBomDoc))
+
+	if config.GetBool(utils.FlagHTML) {
+		tmpl, err := template.New(WorkflowID.String()).Parse(htmlTemplate)
+		if err != nil {
+			logger.Debug().Err(err).Msg("error while parsing HTML template")
+			return nil, errors.NewInternalError("Error parsing HTML template.").SnykError
+		}
+
+		var html strings.Builder
+		if err := tmpl.Execute(&html, aiBomDoc); err != nil {
+			logger.Debug().Err(err).Msg("error while executing HTML template")
+			return nil, errors.NewInternalError("Error executing HTML template.").SnykError
+		}
+
+		workflowData = newWorkflowData("text/html", []byte(html.String()))
+	}
+
+	return []workflow.Data{workflowData}, nil
 }
 
-func extractAiBomFromResult(response *code.AnalysisResponse, logger *zerolog.Logger) (output []workflow.Data, err error) {
+func extractAiBomFromResult(response *code.AnalysisResponse, logger *zerolog.Logger) (output string, err error) {
 	if len(response.Sarif.Runs) != 1 {
 		logger.Debug().Msgf("Failed to extract AI-BOM from result, %d runs in result, expected 1", len(response.Sarif.Runs))
-		return nil, goErrors.New("Failed to extract AI-BOM from result.")
+		return "", goErrors.New("Failed to extract AI-BOM from result.")
 	}
 	if len(response.Sarif.Runs[0].Results) != 1 {
 		logger.Debug().Msgf("Failed to extract AI-BOM from result, %d results in Runs[0], expected 1", len(response.Sarif.Runs[0].Results))
-		return nil, goErrors.New("Failed to extract AI-BOM from result.")
+		return "", goErrors.New("Failed to extract AI-BOM from result.")
 	}
-	return []workflow.Data{newWorkflowData("application/json", []byte(response.Sarif.Runs[0].Results[0].Message.Text))}, nil
+	return response.Sarif.Runs[0].Results[0].Message.Text, nil
 }
 
 //nolint:ireturn // Unable to change return type of external library
