e244d7ddda44ae4721ee7212518b75a0db023903
pkey <paulius.kutka@snyk.io>
2025-10-14 15:01:46 +0200

    fix: cleanup

--- internal/commands/redteam/redteam_test.go
+++ internal/commands/redteam/redteam_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -28,21 +27,11 @@ const (
 
 // MockRedTeamClient implements the RedTeamClient interface for testing.
 type MockRedTeamClient struct {
-	scanData           []redteamclient.AIScan
-	scanResults        redteamclient.GetAIVulnerabilitiesResponseData
-	createError        error
-	getError           error
-	listError          error
-	resultsError       error
-	checkEndpointError error
-}
-
-func (m *MockRedTeamClient) CheckAPIAvailability(_ context.Context, _ string) *errors.Error {
-	if m.createError != nil {
-		err := snyk_common_errors.NewServerError(m.createError.Error())
-		return &err
-	}
-	return nil
+	scanData     []redteamclient.AIScan
+	scanResults  redteamclient.GetAIVulnerabilitiesResponseData
+	createError  error
+	getError     error
+	resultsError error
 }
 
 func (m *MockRedTeamClient) RunScan(_ context.Context, _ string, _ *redteamclient.RedTeamConfig) (string, *errors.Error) {
@@ -67,48 +56,6 @@ func (m *MockRedTeamClient) GetScanResults(_ context.Context, _, _ string) (redt
 		return redteamclient.GetAIVulnerabilitiesResponseData{}, &err
 	}
 	return m.scanResults, nil
-}
-
-func (m *MockRedTeamClient) ListScans(_ context.Context, _ string) ([]redteamclient.AIScan, *errors.Error) {
-	if m.listError != nil {
-		err := snyk_common_errors.NewServerError(m.listError.Error())
-		return nil, &err
-	}
-	return m.scanData, nil
-}
-
-func (m *MockRedTeamClient) ValidateTarget(_ context.Context, _ string, _ *redteamclient.RedTeamConfig) *errors.Error {
-	if m.checkEndpointError != nil {
-		err := snyk_common_errors.NewServerError(m.checkEndpointError.Error())
-		return &err
-	}
-	return nil
-}
-
-func TestRunRedTeamWorkflow_GetScanCommand(t *testing.T) {
-	ictx := frameworkmock.NewMockInvocationContext(t)
-	ictx.GetConfiguration().Set(experimentalKey, true)
-	ictx.GetConfiguration().Set("organization", testOrgID)
-	ictx.GetConfiguration().Set("scan-id", "test-scan-id")
-
-	mockClient := &MockRedTeamClient{
-		scanData: []redteamclient.AIScan{
-			{
-				ID:      uuid.New().String(),
-				Status:  redteamclient.AIScanStatusCompleted,
-				Created: &[]time.Time{time.Now()}[0],
-			},
-		},
-	}
-
-	originalArgs := os.Args
-	os.Args = []string{"snyk", "redteam", "get", "scan"}
-	defer func() { os.Args = originalArgs }()
-
-	results, err := redteam.RunRedTeamWorkflow(ictx, mockClient)
-	require.NoError(t, err)
-	assert.Len(t, results, 1)
-	assert.Equal(t, "text/plain", results[0].GetContentType())
 }
 
 func TestRunRedTeamWorkflow_CreateScanCommand(t *testing.T) {
@@ -137,8 +84,8 @@ target:
 			ID: uuid.New().String(),
 			Results: []redteamclient.AIVulnerability{
 				{
-					ID:  "test-vid",
-					URL: "test-url",
+					ID:  "test-vulnerability-id",
+					URL: "test-vulnerability-url",
 				},
 			},
 		},
@@ -152,6 +99,9 @@ target:
 	require.NoError(t, err)
 	assert.Len(t, results, 1)
 	assert.Equal(t, "application/json", results[0].GetContentType())
+	payload, _ := results[0].GetPayload().([]byte)
+	assert.Contains(t, string(payload), "test-vulnerability-id")
+	assert.Contains(t, string(payload), "test-vulnerability-url")
 }
 
 func TestRunRedTeamWorkflow_ExperimentalFlagRequired(t *testing.T) {
@@ -172,7 +122,6 @@ func TestRunRedTeamWorkflow_ExperimentalFlagRequired(t *testing.T) {
 func TestRunRedTeamWorkflow_NoOrgID(t *testing.T) {
 	ictx := frameworkmock.NewMockInvocationContext(t)
 	ictx.GetConfiguration().Set(experimentalKey, true)
-	// Explicitly clear the organization to test the error case
 	ictx.GetConfiguration().Set(configuration.ORGANIZATION, "")
 
 	mockClient := &MockRedTeamClient{}
@@ -183,7 +132,6 @@ func TestRunRedTeamWorkflow_NoOrgID(t *testing.T) {
 
 	_, err := redteam.RunRedTeamWorkflow(ictx, mockClient)
 	require.Error(t, err)
-	// Just check that we get an error, the exact message might be wrapped
 	assert.NotNil(t, err)
 }
 
@@ -216,7 +164,8 @@ func TestHandleRunScanCommand_InvalidYAML(t *testing.T) {
 target:
   name: "Test Target"
   type: api
-  - invalid yaml syntax
+
+  ---- invalid yaml syntax ----
 `
 	err := os.WriteFile("test-invalid.yaml", []byte(configContent), 0o600)
 	require.NoError(t, err)
@@ -373,7 +322,7 @@ target:
 	ictx.GetConfiguration().Set(configFlag, "test-validate-error.yaml")
 
 	mockClient := &MockRedTeamClient{
-		checkEndpointError: assert.AnError,
+		createError: assert.AnError,
 	}
 
 	originalArgs := os.Args
