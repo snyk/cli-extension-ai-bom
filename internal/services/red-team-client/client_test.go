bda9c068f67fcd33e3a77e60a12d60572588415f
pkey <paulius.kutka@snyk.io>
2025-10-14 14:23:22 +0200

    fix: get results happy path

--- internal/services/red-team-client/client_test.go
+++ internal/services/red-team-client/client_test.go
@@ -26,10 +26,6 @@ func isGetAIScanReq(r *http.Request) bool {
 	return r.Method == http.MethodGet && strings.Contains(r.URL.Path, "/ai_scans/")
 }
 
-func isGetAIScanResultsReq(r *http.Request) bool {
-	return r.Method == http.MethodGet && strings.Contains(r.URL.Path, "/ai_scans/%s/vulnerabilities")
-}
-
 const (
 	userAgent = "test-user-agent"
 	orgID     = "test-org-id"
@@ -64,7 +60,6 @@ func TestRedTeamClient_RunScan_Happy(t *testing.T) {
 		case isGetAIScanReq(r):
 			pollCount++
 			if pollCount < 3 {
-				// Return processing status
 				response := redteamclient.GetAIScanResponse{
 					Data: redteamclient.AIScan{
 						Status: redteamclient.AIScanStatusStarted,
@@ -80,26 +75,6 @@ func TestRedTeamClient_RunScan_Happy(t *testing.T) {
 				json.NewEncoder(w).Encode(response)
 			}
 
-		case isGetAIScanResultsReq(r):
-			// Handle getAIBOM
-			response := redteamclient.GetAIVulnerabilitiesResponse{
-				Data: redteamclient.GetAIVulnerabilitiesResponseData{
-					ID: uuid.New().String(),
-					Results: []redteamclient.AIVulnerability{
-						{
-							ID: uuid.New().String(),
-							Definition: redteamclient.AIVulnerabilityDefinition{
-								ID:          uuid.New().String(),
-								Name:        "test-vulnerability",
-								Description: "test-vulnerability-description",
-							},
-							Severity: "high",
-						},
-					},
-				},
-			}
-			json.NewEncoder(w).Encode(response)
-
 		default:
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		}
@@ -114,11 +89,53 @@ func TestRedTeamClient_RunScan_Happy(t *testing.T) {
 		ictx.GetNetworkAccess().GetHttpClient(),
 		ictx.GetUserInterface(),
 		userAgent,
-		server.URL, // Use the test server URL
+		server.URL,
 	)
 
 	result, err := client.RunScan(context.Background(), orgID, &defaultConfig)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, result)
 	assert.Equal(t, scanID, result)
+}
+
+func TestRedTeamClient_GetScanResults_Happy(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/hidden/orgs/test-org-id/ai_scans/test-scan-id/vulnerabilities", r.URL.Path)
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(redteamclient.GetAIVulnerabilitiesResponse{
+			Data: redteamclient.GetAIVulnerabilitiesResponseData{
+				ID: "test-scan-id",
+				Results: []redteamclient.AIVulnerability{
+					{
+						ID: "test_vulnerability_id",
+						Definition: redteamclient.AIVulnerabilityDefinition{
+							ID:          "test_vulnerability_definition_id",
+							Name:        "test_vulnerability_name",
+							Description: "test_vulnerability_description",
+						},
+						Severity: "high",
+					},
+				},
+			},
+		})
+	}))
+
+	defer server.Close()
+
+	logger := loggermock.NewNoOpLogger()
+	ictx := frameworkmock.NewMockInvocationContext(t)
+	client := redteamclient.NewRedTeamClient(
+		logger,
+		ictx.GetNetworkAccess().GetHttpClient(),
+		ictx.GetUserInterface(),
+		userAgent,
+		server.URL,
+	)
+
+	results, err := client.GetScanResults(context.Background(), orgID, "test-scan-id")
+	assert.Nil(t, err)
+	assert.NotEmpty(t, results)
+	assert.Equal(t, "test-scan-id", results.ID)
 }
